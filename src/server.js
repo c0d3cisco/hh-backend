2254249752c0cb00810dd2197c64c902c6f915d1
Heather Holcomb <holcombheather@gmail.com>
2023-07-26 00:18:11 -0700

    update all queries for the admin data dashboard

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@
 // 3rd Party Resources
 const express = require('express');
 const cors = require('cors');
-
+const Sequelize = require('sequelize');
+const { Op } = require('sequelize');
 const bearerAuth = require('./auth/middleware/bearer');
 const acl = require('./auth/middleware/acl');
 
@@ -16,7 +17,6 @@ const logger = require('./middleware/logger.js');
 const Routes = require('./routes/index.js');
 const { userAuthModel } = require('./models/index.js');
 const { checkin, users, userAuth } = require('./models/index.js');
-const { Op } = require('sequelize');
 
 // Prepare the express app
 const app = express();
@@ -304,6 +304,324 @@ app.get('/totalDailyCheckins', checkJwt, async (req, res, next) => {
     res.status(500).send('Error getting unique user checkin count');
   }
 });
+
+// Query for Dashboard: Weekly Average Check-ins
+app.get('/weeklyAverage', checkJwt, async (req, res, next) => {
+  try {
+    let date_start = req.query.date_start;
+
+    // parse date string into Date object
+    let start = new Date(date_start);
+    let end = new Date(); // set the end date to the current date/time
+
+    // adjust start date to the most recent Monday
+    start.setDate(start.getDate() - start.getDay() + 1);
+
+    // create an array to store the results for each week
+    let weeklyAverages = [];
+
+    // iterate over weeks
+    for (let weekStart = start; weekStart < end; weekStart.setDate(weekStart.getDate() + 7)) {
+      let weekEnd = new Date(weekStart);
+      weekEnd.setDate(weekEnd.getDate() + 6);
+
+      const checkins = await checkin.findAll({
+        where: {
+          timeOut: {
+            [Op.not]: null,
+          },
+          timeIn: {
+            [Op.gte]: weekStart,
+            [Op.lte]: weekEnd,
+          },
+        },
+      });
+
+      const userTimeDifference = {};
+
+      checkins.forEach((checkin) => {
+        const { userId, timeIn, timeOut } = checkin;
+
+        if (!userTimeDifference[userId]) {
+          userTimeDifference[userId] = 0;
+        }
+
+        const differenceInMs = new Date(timeOut) - new Date(timeIn);
+        userTimeDifference[userId] += differenceInMs;
+      });
+
+      let totalDifferenceInMs = 0;
+      let numberOfUsers = 0;
+
+      for (const userId in userTimeDifference) {
+        const differenceInMs = userTimeDifference[userId];
+        totalDifferenceInMs += differenceInMs;
+        numberOfUsers++;
+      }
+
+      let averageTimePerUserInHours = 0;
+      let remainingMinutes = 0;
+
+      if (numberOfUsers > 0) {
+        const averageTimePerUserInMs = totalDifferenceInMs / numberOfUsers;
+        const averageTimePerUserInMinutes = averageTimePerUserInMs / (1000 * 60);
+        averageTimePerUserInHours = Math.floor(averageTimePerUserInMinutes / 60);
+        remainingMinutes = Math.floor(averageTimePerUserInMinutes % 60);
+      }
+
+      console.log(`Week starting ${weekStart}: Average check-in time is ${averageTimePerUserInHours} hours and ${remainingMinutes} minutes`);
+
+      // add the results for this week to the array
+      weeklyAverages.push({
+        weekStart: weekStart.toISOString(),
+        averageTimeInHours: averageTimePerUserInHours,
+        averageTimeInMinutes: remainingMinutes
+      });
+    }
+
+    // send the results array back to the client
+    res.status(200).send(weeklyAverages);
+
+  } catch (error) {
+    console.log(error.message || error);
+    res.status(500).send('error getting weeklyAverage');
+  }
+});
+
+
+
+// Query for Dashboard: YTD Check-ins
+app.get('/totalYearlyCheckins', checkJwt, async (req, res, next) => {
+  try {
+    let start = new Date(new Date().getFullYear(), 0, 1); // Start of the year
+    let end = new Date(); // Today's date
+
+    const checkinCount = await checkin.count({
+      where: {
+        timeOut: {
+          [Op.not]: null,
+        },
+        timeIn: {
+          [Op.gte]: start,
+          [Op.lte]: end,
+        },
+      },
+    });
+
+    res.status(200).send({totalCheckinsYearToDate: checkinCount});
+  } catch (error) {
+    console.log(error.message || error);
+    res.status(500).send('Error getting total checkin count for the year');
+  }
+});
+
+// Query for Dashboard: Total Hours
+app.get('/totalCheckinHours', checkJwt, async (req, res, next) => {
+  try {
+    const checkins = await checkin.findAll({
+      where: {
+        timeOut: {
+          [Op.not]: null,
+        },
+      },
+    });
+
+    let totalHours = 0;
+
+    checkins.forEach((checkin) => {
+      const { timeIn, timeOut } = checkin;
+      const differenceInMs = new Date(timeOut) - new Date(timeIn);
+      const differenceInHours = differenceInMs / (1000 * 60 * 60);
+      totalHours += differenceInHours;
+    });
+
+    res.status(200).send({ totalHours: totalHours });
+  } catch (error) {
+    console.log(error.message || error);
+    res.status(500).send('Error getting total checkin hours');
+  }
+});
+
+
+// Query for Dashboard Chart: Total Hours for Check-ins by day
+app.get('/totalHoursPerWeek', checkJwt, async (req, res, next) => {
+  try {
+    let date_end = new Date();
+    let date_start = new Date();
+    date_start.setDate(date_end.getDate() - 7); // Adjust this value to change the time span
+
+    const checkins = await checkin.findAll({
+      where: {
+        timeOut: {
+          [Op.not]: null,
+        },
+        timeIn: {
+          [Op.gte]: date_start,
+          [Op.lte]: date_end,
+        },
+      },
+    });
+
+    let totalDifferenceInMs = 0;
+
+    checkins.forEach((checkin) => {
+      const { timeIn, timeOut } = checkin;
+      const differenceInMs = new Date(timeOut) - new Date(timeIn);
+      totalDifferenceInMs += differenceInMs;
+    });
+
+    const totalDifferenceInHours = totalDifferenceInMs / (1000 * 60 * 60);
+
+    res.status(200).send({totalHours: totalDifferenceInHours});
+
+  } catch (error) {
+    console.log(error.message || error);
+    res.status(500).send('error getting totalHoursPerWeek');
+  }
+});
+
+
+
+// Query for Dashboard Chart: Total Number of Unique Check-ins by day
+app.get('/uniqueUsersPerWeek', checkJwt, async (req, res, next) => {
+  try {
+    let date_end = new Date();
+    let date_start = new Date();
+    date_start.setDate(date_end.getDate() - 7); // Adjust this value to change the time span
+
+    const checkinCount = await checkin.count({
+      distinct: true,
+      col: 'userId',
+      where: {
+        timeOut: {
+          [Op.not]: null,
+        },
+        timeIn: {
+          [Op.gte]: date_start,
+          [Op.lte]: date_end,
+        },
+      },
+    });
+
+    res.status(200).send({uniqueCheckinUsers: checkinCount});
+  } catch (error) {
+    console.log(error.message || error);
+    res.status(500).send('error getting uniqueUsersPerWeek');
+  }
+});
+
+
+
+// Query for Pie Chart: Total Hours A Member was Checked In on a Given Day (Compare to total hours of operations on a given day)
+app.get('/totalActiveTime', checkJwt, async (req, res, next) => {
+  try {
+    const date = req.query.date;
+
+    const earliestCheckIn = await checkin.findOne({
+      where: {
+        timeIn: {
+          [Op.gte]: new Date(`${date}T00:00:00.000Z`),
+          [Op.lte]: new Date(`${date}T23:59:59.999Z`),
+        },
+      },
+      order: [
+        ['timeIn', 'ASC']
+      ],
+    });
+
+    const latestCheckOut = await checkin.findOne({
+      where: {
+        timeOut: {
+          [Op.gte]: new Date(`${date}T00:00:00.000Z`),
+          [Op.lte]: new Date(`${date}T23:59:59.999Z`),
+        },
+      },
+      order: [
+        ['timeOut', 'DESC']
+      ],
+    });
+
+    if (!earliestCheckIn || !latestCheckOut) {
+      res.status(200).send({activeHours: 0});
+      return;
+    }
+
+    const timeDifferenceInMs = new Date(latestCheckOut.timeOut) - new Date(earliestCheckIn.timeIn);
+    const timeDifferenceInHours = timeDifferenceInMs / (1000 * 60 * 60);
+
+    res.status(200).send({activeHours: timeDifferenceInHours});
+  } catch (error) {
+    console.log(error.message || error);
+    res.status(500).send('Error getting total active hours');
+  }
+});
+
+// Query for Dashboard: Average moodIn for a given day
+app.get('/averageMoodIn', checkJwt, async (req, res, next) => {
+  try {
+    let date = req.query.date;
+    let moodMapping = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5};  // Map ENUM to integer
+
+    const moodInData = await checkin.findAll({
+      attributes: ['moodIn'],
+      where: {
+        timeIn: {
+          [Op.between]: [new Date(`${date}T00:00:00.000Z`), new Date(`${date}T23:59:59.999Z`)],
+        },
+      },
+    });
+
+    // Map the fetched ENUMs to integers and calculate the average
+    let sumMoodIn = 0;
+    for (let moodInObj of moodInData) {
+      sumMoodIn += moodMapping[moodInObj.moodIn];
+    }
+    let avgMoodIn = sumMoodIn / moodInData.length;
+
+    res.status(200).send({ averageMoodIn: avgMoodIn });
+  } catch (error) {
+    console.log(error.message || error);
+    res.status(500).send('Error getting average moodIn for a day');
+  }
+});
+
+
+
+// Query for Dashboard: Average moodOut for a given day
+app.get('/averageMoodOut', checkJwt, async (req, res, next) => {
+  try {
+    let date = req.query.date;
+    let moodMapping = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5};  // Map ENUM to integer
+
+    const moodOutData = await checkin.findAll({
+      attributes: ['moodOut'],
+      where: {
+        timeOut: {
+          [Op.between]: [new Date(`${date}T00:00:00.000Z`), new Date(`${date}T23:59:59.999Z`)],
+        },
+      },
+    });
+
+    // Map the fetched ENUMs to integers and calculate the average
+    let sumMoodOut = 0;
+    for (let moodOutObj of moodOutData) {
+      if (moodOutObj.moodOut !== null) {
+        sumMoodOut += moodMapping[moodOutObj.moodOut];
+      }
+    }
+    let avgMoodOut = sumMoodOut / moodOutData.length;
+
+    res.status(200).send({ averageMoodOut: avgMoodOut });
+  } catch (error) {
+    console.log(error.message || error);
+    res.status(500).send('Error getting average moodOut for a day');
+  }
+});
+
+
+
+
+
 
 
 // proof of life
